e77f0a3b683a45daf378cb8b17e32bb8bd96f4b3
Max Prettyjohns <heshoots9999@gmail.com>
2016-06-20 21:08:56 +0100

    Implement achievement admin React form into route

--- src/server/routes/achievement.js
+++ src/server/routes/achievement.js
@@ -1,12 +1,20 @@
 'use strict';
 
 const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+const AchievementForm = React.createFactory(
+	require('../../client/components/forms/achievementAdmin.jsx')
+);
 
 const router = express.Router();
 
 router.get('/admin', (req, res) => {
-	const markup = {};
-	res.render('achievement', {})
+	const markup = ReactDOMServer.renderToString(AchievementForm({}));
+	res.render('achievement', {
+		markup
+	})
 });
 
 module.exports = router;
