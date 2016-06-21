7b367fe8ce761ef82af88f700eaca7ec4bfd094c
Max Prettyjohns <heshoots9999@gmail.com>
2016-06-21 20:40:10 +0100

    Fix style of achievement admin page

--- src/client/components/forms/achievementAdmin.jsx
+++ src/client/components/forms/achievementAdmin.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 const Input = require('react-bootstrap').Input;
 const Button = require('react-bootstrap').Button;
 const request = require('superagent-bluebird-promise');
+
 module.exports = React.createClass({
 	displayName: 'AchievementAdminForm',
 	handleSubmit(event) {
@@ -11,18 +12,18 @@ module.exports = React.createClass({
 			editor: this.editor,
 			achievement: this.achievement,
 			title: this.title
-		}
+		};
 		this.setState({waiting: true});
 
 		request.post('/achievement/admin/handler')
 			.send(data).promise()
-			.then((res) => {
+			.then(() => {
 				window.location.href = '/blah';
-			})
+			});
 	},
 	render() {
 		'use strict';
-		
+
 		return (
 			<form
 				className="form-horizontal"
@@ -41,14 +42,14 @@ module.exports = React.createClass({
 					ref={(ref) => this.achievement = ref}
 					type="textarea"
 					wrapperClassName="col-md-9"
-					/>
+				/>
 				<Input
 					label="Title"
 					labelClassName="col-md-3"
 					ref={(ref) => this.title = ref}
 					type="textarea"
 					wrapperClassName="col-md-9"
-					/>
+				/>
 				<div className="form-group">
 					<div className="col-md-4 col-md-offset-4">
 						<Button
@@ -63,6 +64,5 @@ module.exports = React.createClass({
 				</div>
 			</form>
 		);
-		
 	}
-})
+});
