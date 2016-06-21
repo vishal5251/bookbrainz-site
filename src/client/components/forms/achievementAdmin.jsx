b289b4b0822768194dd2c3994368274fe46b3fae
Max Prettyjohns <heshoots9999@gmail.com>
2016-06-21 19:09:08 +0100

    Handle submits from achievement admin system

--- src/client/components/forms/achievementAdmin.jsx
+++ src/client/components/forms/achievementAdmin.jsx
@@ -1,15 +1,32 @@
 const React = require('react');
 const Input = require('react-bootstrap').Input;
 const Button = require('react-bootstrap').Button;
-
+const request = require('superagent-bluebird-promise');
 module.exports = React.createClass({
 	displayName: 'AchievementAdminForm',
+	handleSubmit(event) {
+		'use strict';
+		event.preventDefault();
+		const data = {
+			editor: this.editor,
+			achievement: this.achievement,
+			title: this.title
+		}
+		this.setState({waiting: true});
+
+		request.post('/achievement/admin/handler')
+			.send(data).promise()
+			.then((res) => {
+				window.location.href = '/blah';
+			})
+	},
 	render() {
 		'use strict';
 		
 		return (
 			<form
 				className="form-horizontal"
+				onSubmit={this.handleSubmit}
 			>
 				<Input
 					label="Editor"
@@ -17,7 +34,7 @@ module.exports = React.createClass({
 					ref={(ref) => this.editor = ref}
 					type="textarea"
 					wrapperClassName="col-md-9"
-					/>
+				/>
 				<Input
 					label="Achievement"
 					labelClassName="col-md-3"
