13b0bcc359a90f64e4ec2df6fcedfa2becd97c78
Max Prettyjohns <heshoots9999@gmail.com>
2016-06-22 19:50:31 +0100

    Output lists of titles and achievements for admin

--- src/server/routes/achievement.js
+++ src/server/routes/achievement.js
@@ -13,13 +13,62 @@ const AchievementForm = React.createFactory(
 	require('../../client/components/forms/achievementAdmin.jsx')
 );
 
+const Editor = require('bookbrainz-data').Editor;
+const AchievementType = require('bookbrainz-data').AchievementType;
+const TitleType = require('bookbrainz-data').TitleType;
+
 const router = express.Router();
 
 router.get('/admin', (req, res) => {
-	const markup = ReactDOMServer.renderToString(AchievementForm({}));
-	res.render('achievement', {
-		markup
-	});
+	const EditorJSONPromise = new Editor()
+		.orderBy('name')
+		.fetchAll()
+		.then((editors) => {
+			return editors.map((editor) => {
+				const editorJSON = editor.toJSON();
+				return {
+					id: editorJSON.id,
+					name: editorJSON.name
+				}
+			});
+		});
+
+	const AchievementJSONPromise = new AchievementType()
+		.orderBy('name')
+		.fetchAll()
+		.then((achievements) => {
+			const achievementList = achievements.toJSON();
+			achievementList.unshift({id: 'none', name: 'No Achievement'});
+			console.log(achievementList);
+			return achievementList;
+		});
+
+	const TitleJSONPromise = new TitleType()
+		.orderBy('title')
+		.fetchAll()
+		.then((titles) => {
+			const titleList = titles.toJSON();
+			titleList.unshift({id: 'none', title: 'No Title'});
+			return titleList;
+		});
+
+	Promise.join(EditorJSONPromise,
+		AchievementJSONPromise,
+		TitleJSONPromise,
+		(editorJSON, achievementJSON, titleJSON) => {
+			const props = {
+				editor: editorJSON,
+				achievement: achievementJSON,
+				title: titleJSON
+			}
+			const markup = ReactDOMServer.renderToString(
+				AchievementForm(props)
+			);
+			res.render('achievement', {
+				props,
+				markup
+			});
+		})
 });
 
 // XXX: need to authenticate admins once it is implemented
